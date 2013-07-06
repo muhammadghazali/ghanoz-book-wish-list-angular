ecbf1817f53b04aecbadcc130b1d6b6aa5b85a97
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-07-06 14:03:02 +0700

    Scaffolding the app using generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ghanozBookWishListAngularApp')
+  .controller('MainCtrl', function($scope) {
+    $scope.books = [
+      '97 Things Every Programmer Should Know',
+      'NoSQL Distilled A Brief Guide to the Emerging World of Polyglot' +
+        ' Persistence',
+      'The Agile Samurai',
+      'JavaScript Patterns',
+      'JavaScript The Definitive Guide, 6th Edition',
+      'Learning Node',
+      'Maintainable JavaScript',
+      'Node.js in Action',
+      'Node Up and Running',
+      'Node Cookbook'
+    ];
+  });
